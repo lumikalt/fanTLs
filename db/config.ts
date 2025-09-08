cb6e37f08a2692643f0ecb64439bbe371236b1e1
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-09-08 23:58:36 +0100

    db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,59 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW } from "astro:db";
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, autoIncrement: true, unique: true }),
+    username: column.text(),
+    email: column.text(),
+    createdAt: column.date({ default: NOW })
+  }
+});
+
+const Comment = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, autoIncrement: true, unique: true }),
+    authorId: column.number({ references: () => User.columns.id }),
+
+    targetType: column.text({ enum: ["user", "novel", "chapter"] }),
+    targetId: column.number(),
+
+    // for replies - set targetType and targetId to the same as the parent, and the parentId here
+    parentId: column.number({
+      optional: true,
+      references: () => Comment.columns.id
+    }),
+
+    text: column.text(),
+    createdAt: column.date({ default: NOW })
+  }
+});
+
+// Novels table
+const Novel = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, autoIncrement: true, unique: true }),
+    name: column.text(),
+    authorId: column.number({
+      references: () => User.columns.id,
+      optional: true
+    }),
+    createdAt: column.date({ default: NOW })
+  }
+});
+
+const Chapter = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true, autoIncrement: true, unique: true }),
+    novelId: column.number({ references: () => Novel.columns.id }),
+    authorId: column.number({
+      optional: true,
+      references: () => User.columns.id
+    }),
+    createdAt: column.date({ default: NOW }),
+    chapter: column.number()
+  }
+});
+
 export default defineDb({
-  tables: {}
+  tables: { User, Comment, Novel, Chapter }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,40 @@
-import { db } from 'astro:db';
+import { db, User, Comment, Novel } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(User).values([
+    { username: "alice", email: "alice@example.com" },
+    { username: "bob", email: "bob@example.com" },
+    { username: "charlie", email: "charlie@example.com" }
+  ]);
+
+  await db.insert(Novel).values([
+    { id: 1, name: "some_name", authorId: 2 },
+    { name: "another", authorId: 1 }
+  ]);
+
+  await db.insert(Comment).values([
+    {
+      id: 77,
+      authorId: 1,
+      text: "This novel was so beautiful, it was as if some occult hand had penned it.\n\n*Bravo*, writer.",
+      targetType: "novel",
+      targetId: 1
+    },
+    {
+      id: 99,
+      authorId: 2,
+      text: "Deep!",
+      targetType: "novel",
+      targetId: 1,
+      parentId: 77
+    },
+    {
+      authorId: 3,
+      text: "what",
+      targetType: "novel",
+      targetId: 1,
+      parentId: 77
+    }
+  ]);
 }
