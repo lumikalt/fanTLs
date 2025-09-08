21add09b7405565a635f5f790831775b0bf5470b
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-09-08 23:56:52 +0100

    Remove math stuffs, maybe make a fork that still has them...?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/consts";
 
 import mdx from "@astrojs/mdx";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
 import rehypeBetterTooltips from "./src/utils/rehype-better-tooltips";
 import rehypeIdent from "./src/utils/rehype-ident";
 
@@ -21,8 +19,8 @@ export default defineConfig({
   site: SITE_URL,
   integrations: [mdx(), sitemap(), db()],
   markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex, rehypeBetterTooltips, rehypeIdent],
+    remarkPlugins: [],
+    rehypePlugins: [rehypeBetterTooltips, rehypeIdent],
   },
   adapters: [
     vercel({
